0a44ea6ae7a5bafadd19b4d5bd8542d4f97028b8
taittn <taittn@hitachiconsulting.com>
2020-07-07 15:21:47 +0700

    add test crlf false

---
+++ FrontEnd/src/app/user/acount/acount.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-acount',
+  templateUrl: './acount.component.html',
+  styleUrls: ['./acount.component.scss']
+})
+export class AcountComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- FrontEnd/src/app/user/user.module.ts
+++ FrontEnd/src/app/user/user.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UserComponent } from './user/user.component';
+import { AcountComponent } from './acount/acount.component';
 
 
 
 @NgModule({
-  declarations: [UserComponent],
+  declarations: [UserComponent, AcountComponent],
   imports: [
     CommonModule
   ],
