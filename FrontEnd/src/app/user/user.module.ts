c908c62b3736043d52f254e57aa1e7041d21039d
taittn <taittn@hitachiconsulting.com>
2020-07-07 17:00:03 +0700

    add lf or

--- FrontEnd/src/app/user/user.module.ts
+++ FrontEnd/src/app/user/user.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UserComponent } from './user/user.component';
 import { AcountComponent } from './acount/acount.component';
+import { RolesComponent } from './roles/roles.component';
 
 
 
 @NgModule({
-  declarations: [UserComponent, AcountComponent],
+  declarations: [UserComponent, AcountComponent, RolesComponent],
   imports: [
     CommonModule
   ],
