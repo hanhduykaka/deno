4bf420563fd0ce9a82899acd14c3f3402088815c
taittn <taittn@hitachiconsulting.com>
2020-07-08 10:18:51 +0700

    add java

--- FrontEnd/src/app/user/user.module.ts
+++ FrontEnd/src/app/user/user.module.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { UserComponent } from './user/user.component';
 import { AcountComponent } from './acount/acount.component';
 import { RolesComponent } from './roles/roles.component';
+import { TestComponent } from './test/test.component';
 
 
 
 @NgModule({
-  declarations: [UserComponent, AcountComponent, RolesComponent],
+  declarations: [UserComponent, AcountComponent, RolesComponent, TestComponent],
   imports: [
     CommonModule
   ],
